6f4191857cb9372402a3307ccec1a20b0a767226
RaziurC4S5E <105146023+RaziurC4S5E@users.noreply.github.com>
2025-11-11 22:14:13 +0600

    added the complete code for commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+SLOT MACHINE
 
+As I am learning python, I wanted to build a Slot Machine game.
+
+1.0 This is the first draft of the game
+
+A simple Version 1.0 using List, Random, while and If-Elif-Else logic
---
+++ main.py
@@ -0,0 +1,52 @@
+from random import randint
+
+slot_1 = ["Apple","7","Orange"]
+slot_2 = ["Apple","7","Orange"]
+slot_3 = ["Apple","7","Orange"]
+
+point = 200
+
+print("Welcome to SLOT MACHINE! AS YOU ARE A FIRST TIMER YOU GET 200 COINS FOR FREE.")
+
+run = True
+
+while run:
+    bet_value = int(input("Enter the amount of coin you want to bet: "))
+
+    s1_value = randint(0,2)
+    s2_value = randint(0,2)
+    s3_value = randint(0,2)
+
+
+
+    if slot_1[s1_value] == slot_2[s2_value] == slot_3[s3_value] == '7':
+        print(f"YOU GOT {slot_1[s1_value]} {slot_2[s2_value]} {slot_3[s3_value]} IN THE CENTER")
+        point = point + (bet_value*3)
+        print(f"you win {bet_value*3} coins.")
+    elif slot_1[s1_value] == slot_2[s2_value] == slot_3[s3_value] == "Apple":
+        print(f"YOU GOT 7 7 7 IN THE LOWER ROW")
+        point = point + (bet_value*2)
+        print(f"you win {bet_value*2} coins.")
+    elif slot_1[s1_value] == slot_2[s2_value] == slot_3[s3_value] == "Orange":
+        print(f"YOU GOT 7 7 7 IN THE UPPER ROW")
+        point = point + (bet_value*2)
+        print(f"you win {bet_value*2} coins.")
+    elif (s1_value == 0 and s2_value == 1 and s3_value == 2) or (s1_value == 2 and s2_value == 1 and s3_value == 0):
+        print(f"YOU GOT 7 7 7 IN THE DIAGONALS")
+        point += bet_value
+        print(f"you win {bet_value} coins.")
+    else:
+        print(f"YOU DIDN'T GET 7 7 7 AT ALL")
+        print(f"you lose {bet_value} coins.")
+        point -= bet_value
+    
+    if (point > 0):
+        print(f"after the round your got {point} coins.")
+        play_again = input("Press Y/y if you want to play again  or press N/n if you want to quit: ")
+        if (play_again == 'n' or play_again == "N"):
+            print(f"You got in total {point} coins. GOOD BYE. HOPE TO SEE YOU AGAIN")
+            run = False
+    else:
+        print(f"after the round your got 0 coins.")
+        run = False
+
